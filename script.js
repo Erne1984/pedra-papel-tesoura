0f4dac4c80494e07fff50312eaa1eba8d69e6992
ernesto <ernestofamorim@gmail.com>
2024-01-16 16:38:10 -0300

    some refactorings and UI

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const btnPapper = document.querySelector("#btnPapper");
+const btnScissor = document.querySelector("#btnScissor");
+const btnRock = document.querySelector("#btnRock");
+const roundDisplay = document.querySelector("#round");
+const count = document.querySelector("#count");
+
 function getComputerChoice() {
 
     const choices = ["pedra", "tesoura", "papel"];
@@ -5,74 +11,73 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-function getPlayerChoice() {
-    const choice = prompt("Pedra, papel ou tesoura?");
-
-    validatingInput(choice);
-
-    const choiceFinal = choice.toLocaleLowerCase();
-
-    console.log(choiceFinal);
-
-    return choiceFinal;
-}
-
-function validatingInput(value) {
-
-    if(value === ""){
-        return alert("Please enter something")
-    }
-
-    if((value !== "pedra") && (value !== "tesoura") && (value !== "papel")){
-        return alert("Choose a valid answer!")
-    }
-}
-
 function playRound(playerSelection, computerSelection) {
 
+    let result = "";
+
     if (playerSelection === computerSelection) {
-        return "draw"
+        result = "draw"
     } else if (
         (playerSelection == "pedra" && computerSelection == "tesoura") ||
         (playerSelection == "tesoura" && computerSelection == "papel") ||
         (playerSelection == "papel" && computerSelection == "pedra")) {
 
-        return "player"
+        result = "player"
     } else {
-        return "machine"
+        result = "machine"
+    }
+
+    if (result == "player") {
+        roundDisplay.textContent = "Ponto do player"
+    } else if (result == "machine") {
+        roundDisplay.textContent = "Ponto da maquina"
+    } else {
+        roundDisplay.textContent = "Empate"
     }
 
 }
 
-function game(){
+function game() {
 
     let playerPoints = 0;
     let machinePoints = 0;
-    
-    for(let i = 1; i < 6; i++){
+
+    for (let i = 1; i < 6; i++) {
 
         console.log(`Rodada ${i}`)
 
         const result = playRound(getPlayerChoice(), getComputerChoice());
 
-        if(result == "player"){
+        if (result == "player") {
             playerPoints += 1;
             console.log("Ponto do player")
-        } else if(result == "machine"){
+        } else if (result == "machine") {
             machinePoints += 1;
             console.log("Ponto da maquina")
-        }else{
+        } else {
             console.log("empate")
         }
     }
 
-    if(playerPoints === machinePoints){
-        console.log("Foi um empate") 
-    } else if(playerPoints > machinePoints){
-        console.log("O jogador ganhou") 
-    } else{
-        console.log("A maquina ganhou")     
+    if (playerPoints === machinePoints) {
+        console.log("Foi um empate")
+    } else if (playerPoints > machinePoints) {
+        console.log("O jogador ganhou")
+    } else {
+        console.log("A maquina ganhou")
     }
 }
 
-game();
+
+btnPapper.addEventListener('click', () => {
+    playRound("papel", getComputerChoice())
+
+});
+btnRock.addEventListener('click', () => {
+    playRound("pedra", getComputerChoice())
+
+});
+btnScissor.addEventListener('click', () => {
+    playRound("tesoura", getComputerChoice())
+
+});
